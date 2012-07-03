c87414f55389f75e7dc7e7a6f1c7130e9e13b822
chawarn Homyen <chawarin27@hotmail.com>
2012-07-03 13:42:43 +0700

    เพิ่ม"Programming is fun"

--- src/th/ac/nu/students/u54341628/compro/ch01/HelloWorld.java
+++ src/th/ac/nu/students/u54341628/compro/ch01/HelloWorld.java
@@ -3,6 +3,6 @@ package th.ac.nu.students.u54341628.compro.ch01;
 public class HelloWorld {
 public static void main(String[] args) {
   System.out.println("HelloWorld");
-    
+  System.out.println("Programming is fun");  
     }
 }
